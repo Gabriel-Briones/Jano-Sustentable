fef353fb924b7317361dfbc6a462ac7afb312c17
Gabriel Briones <gamiz08@hotmail.com>
2023-02-01 00:31:27 -0300

    Arreglar logo

--- src/Components/Navbar/Secciones/Secciones.jsx
+++ src/Components/Navbar/Secciones/Secciones.jsx
@@ -3,7 +3,7 @@ export const Secciones = () => {
         <>
             <li className="nav-item">
                 <a className="navbar-brand brand-logo" href="#">
-                    <img src='./favicon.png' alt="Logo Jano Sustentable" width="40" height="40" class="d-inline-block" />
+                    <img src='./favicon.png' alt="Logo Jano Sustentable" width="40" height="40" class="d-inline-block" /> Jano Sustentable
                 </a>
             </li>
             <li className="nav-item">
